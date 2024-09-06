601a303cc7e13495fd12f00da71bec80858fe1d6
Usman Adepoju <adepojuusman465@gmail.com>
2024-09-06 08:25:45 +0100

    Update preload channels

--- preload.js
+++ preload.js
@@ -1,7 +1,12 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-	testScrcpy: () => ipcRenderer.invoke("test-scrcpy"),
+	// App functions channels
 	startScrcpy: () => ipcRenderer.invoke("start-scrcpy"),
 	stopScrcpy: () => ipcRenderer.invoke("stop-scrcpy"),
+	statusUpdate: () => ipcRenderer.invoke("status-update"),
+	customEvent: {
+		on: (channel, listener) => ipcRenderer.on(channel, listener),
+		send: (channel, data) => ipcRenderer.send(channel, data),
+	},
 });
